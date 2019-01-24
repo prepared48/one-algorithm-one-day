bf6b0e8937c97724c133e05e06c05d94aa31b6d8
zhongsb <z20107824@163.com>
2019-01-24 20:56:59 +0800

    add a better solution

--- src/com/parepared/leetCode/Day07SearchInsertPosition35.java
+++ src/com/parepared/leetCode/Day07SearchInsertPosition35.java
@@ -18,15 +18,39 @@ public class Day07SearchInsertPosition35 {
 
     public static void main(String[] args) {
 
-        int [] nums = {1,3};
-        int target = 3;
-        System.out.println(searchInsert(nums, target));
+        int [] nums = {1,3,5,6};
+        int target = 7;
+        System.out.println(searchInsert2(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
         int s = 0;
         int t = nums.length - 1;
         return searchInsert(nums, s, t, target);
+    }
+
+    /**
+     * a better way from the website:
+     * https://leetcode.com/problems/search-insert-position/discuss/224429/My-Java-solution-that-faster-than-100-users
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int searchInsert2(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        int mid = 0;
+        while(l <= r){
+            mid = (l+r)/2;
+            if(nums[mid] < target){
+                l = mid + 1;
+            }else if(nums[mid] > target){
+                r = mid -1;
+            }else{
+                return mid;
+            }
+        }
+        return l;
     }
 
     /**
