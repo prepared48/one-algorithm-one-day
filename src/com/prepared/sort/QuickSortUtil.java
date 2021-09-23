79757293e964b5b2976f424959e1cf751557177e
zhongsb <z20107824@163.com>
2021-09-23 23:38:22 +0800

    重读快速排序代码

--- src/com/prepared/sort/QuickSortUtil.java
+++ src/com/prepared/sort/QuickSortUtil.java
@@ -50,6 +50,9 @@ public class QuickSortUtil {
      * 我们通过游标 i 把 A[p...r-1]分成两部分。A[p...i-1]的元素都是小于 pivot 的，我们暂且叫它“已处理区间”，A[i...r-1]是“未处理区间”。
      * 我们每次都从未处理的区间 A[i...r-1]中取一个元素 A[j]，与 pivot 对比，如果小于 pivot，则将其加入到已处理区间的尾部，也就是 A[i]的位置。
      *
+     *
+     * 如果比pivot小，已处理区间加1，这个值放在这个位置
+     *
      * @param nums
      * @param p
      * @param r
