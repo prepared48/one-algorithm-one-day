ba32ae875db35e51c7a786ec942171f71655a818
zhongsb <z20107824@163.com>
2021-09-26 23:41:14 +0800

    复习hash相关算法题

--- src/com/prepared/leetcode/hash/Day28RansomLetter383.java
+++ src/com/prepared/leetcode/hash/Day28RansomLetter383.java
@@ -3,6 +3,8 @@ package com.prepared.leetcode.hash;
 /**
  * 383. 赎金信
  *
+ * 给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+ *
  * @Author: zhongshibo
  * @Date: 2021/6/3 18:57
  */
